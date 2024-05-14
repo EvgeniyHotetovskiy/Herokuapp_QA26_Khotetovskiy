2ef761b0e20bbd93af0a35e721674dd3be6c27b4
EvgeniyHotetovskiy <squaer.r@mail.ru>
2024-05-14 13:02:36 +0300

    Remake HomeWork2

--- src/test/java/HomeWork/DropdownList.java
+++ src/test/java/HomeWork/DropdownList.java
@@ -2,6 +2,7 @@ package HomeWork;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.util.List;
@@ -14,9 +15,9 @@ public class DropdownList extends PreCondition {
         Assert.assertEquals(Rows.size(), 3);
         Rows.get(1).click();
         List<WebElement> Rows1 = driver.findElements(By.cssSelector("#dropdown > option"));
-        Assert.assertTrue(Rows1.get(1).getAttribute("selected").equals("true"));
+        Assert.assertTrue(Rows1.get(1).isSelected());
         Rows.get(2).click();
         List<WebElement> Rows2 = driver.findElements(By.cssSelector("#dropdown > option"));
-        Assert.assertTrue(Rows2.get(2).getAttribute("selected").equals("true"));
+        Assert.assertTrue(Rows2.get(2).isSelected());
     }
 }
